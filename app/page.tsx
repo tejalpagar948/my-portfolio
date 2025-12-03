865a6e07df68a4c9b65c5b7c3d5d894a74f7b8ae
ptejalaxioned <tejalp@axioned.com>
2025-12-03 11:04:58 +0530

    remove unanted code

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-// "use client";
 import HeroSection from '@/components/sections/hero-section';
-// import { useState } from 'react';
 import AboutUs from '@/components/sections/about-us-section';
 import KeySkillsSection from '@/components/sections/key-skills-section';
 import SkillsSection from '@/components/sections/skills-section';
@@ -8,21 +6,13 @@ import ResumeSection from '@/components/sections/resume-section';
 import ReviewSection from '@/components/sections/review-section';
 import ProjectSection from '@/components/sections/project-section';
 import ContactSection from '@/components/sections/contactSection';
-import { getHomePage } from '@/sanity/lib/queries';
 import { client } from '@/sanity/lib/client';
 import type { Page } from '@/sanity.types';
-
-import FeedbackForm from '@/components/feedback';
-
-import Link from 'next/link';
-
-import FeedbackDisplay from '@/components/feedbackDisplay';
 
 export default async function Home() {
   const data = await client.fetch<Page>(`*[_type == "page"][0]`);
   const hero = data?.content?.find((item) => item._type === 'heroSection');
 
-  // const [showFeedbackForm, setShowFeedbackForm] = useState(false);
   return (
     <>
       {hero && <HeroSection value={hero} />}
