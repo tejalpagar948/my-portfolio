c8c8eb9565f43858bf1ad71893529c014cdb2a0c
ptejalaxioned <tejalp@axioned.com>
2025-12-06 18:59:24 +0530

    fix code

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,20 +1,17 @@
+export const projectId =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
+  (() => {
+    throw new Error("Missing: NEXT_PUBLIC_SANITY_PROJECT_ID");
+  })();
+
+export const dataset =
+  process.env.NEXT_PUBLIC_SANITY_DATASET ||
+  (() => {
+    throw new Error("Missing: NEXT_PUBLIC_SANITY_DATASET");
+  })();
+
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-11-16'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-10-01";
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
-
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
-
-function assertValue<T>(v: T | undefined, errorMessage: string): T {
-  if (v === undefined) {
-    throw new Error(errorMessage)
-  }
-
-  return v
-}
+export const useCdn =
+  process.env.NEXT_PUBLIC_SANITY_USE_CDN === "true";
