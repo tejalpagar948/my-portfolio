3938079bbfb1b1484f66a3801b75bf05d62bb7c0
ptejalaxioned <tejalp@axioned.com>
2025-12-06 16:33:49 +0530

    fix code

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import HeroSection from '@/components/sections/hero-section';
 import AboutUs from '@/components/sections/about-us-section';
 import KeySkillsSection from '@/components/sections/key-skills-section';
