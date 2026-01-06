c6a9fae423bd9f72ba95c3237f9590c7dc7665a5
tejalpagar948 <tejalpagar948@gmail.com>
2026-01-07 02:24:12 +0530

    Merge remote main into local project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import 'slick-carousel/slick/slick.css';
@@ -13,38 +12,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="font-elmessiri">{children}</body>
-=======
-import './globals.css';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import ClientLayout from './client-layout';
-import AOSProvider from '@/components/animation/aosProvider';
-import { getHeader } from '@/sanity/lib/queries';
-import { getFooter } from '@/sanity/lib/queries';
-import { getSiteConfig } from '@/sanity/lib/queries';
-
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  // ✅ Fetch here (SERVER component)
-  const header = await getHeader();
-  const footer = await getFooter();
-  if (!footer?.content) return null;
-
-  const siteConfig = await getSiteConfig();
-  if (!siteConfig) return null;
-
-  return (
-    <html lang="en">
-      <body className="text-custom-white">
-        <AOSProvider />
-        <ClientLayout header={header} footer={footer} siteConfig={siteConfig}>
-          {children}
-        </ClientLayout>
-      </body>
->>>>>>> 8c5257b627f2e18cf7d28436007025e415a44ec9
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import HeroSection from './component/sections/hero-section';
 import ExploreGrid from './component/sections/explore-grid';
 import ExperienceAdventure from './component/sections/experience-adventure';
@@ -23,56 +22,6 @@ export default function Home() {
       <BlogSection />
       <PromoHeroSection />
       <SiteFooter />
-=======
-import HeroSection from '@/components/sections/hero-section';
-import AboutSection from '@/components/sections/about-us-section';
-import SkillsSection from '@/components/sections/skills-section';
-import KeySkillsSection from '@/components/sections/key-skills-section';
-import ResumeSection from '@/components/sections/resume-section';
-import ReviewSection from '@/components/sections/review-section';
-import ProjectSection from '@/components/sections/project-section';
-import ContactSection from '@/components/sections/contactSection';
-
-import { getFooter, getHomePage } from '@/sanity/lib/queries';
-
-export default async function Home() {
-  const data = await getHomePage();
-
-  if (!data?.content) return null;
-
-  return (
-    <>
-      {data.content.map((section: any) => {
-        switch (section._type) {
-          case 'heroSection':
-            return <HeroSection key={section._key} value={section} />;
-
-          case 'aboutSection':
-            return <AboutSection key={section._key} value={section} />;
-
-          case 'skillSection':
-            return <KeySkillsSection key={section._key} value={section} />;
-
-          case 'skillProficiencySection':
-            return <SkillsSection key={section._key} value={section} />;
-
-          case 'resumeSection':
-            return <ResumeSection key={section._key} value={section} />;
-
-          // case 'reviewsSection':
-          //   return <ReviewSection key={section._key} value={section} />;
-
-          case 'projectSection':
-            return <ProjectSection key={section._key} value={section} />;
-
-          case 'contactSection':
-            return <ContactSection key={section._key} value={section} />;
-
-          default:
-            return null;
-        }
-      })}
->>>>>>> 8c5257b627f2e18cf7d28436007025e415a44ec9
     </>
   );
 }
