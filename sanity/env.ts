c016bc06723daba5ae8a028a55db3d80d32081e1
tejalpagar948 <tejalpagar948@gmail.com>
2026-03-10 15:29:24 +0530

    fix sanity issue

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,17 +1,4 @@
-export const projectId =
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
-  (() => {
-    throw new Error("Missing: NEXT_PUBLIC_SANITY_PROJECT_ID");
-  })();
-
-export const dataset =
-  process.env.NEXT_PUBLIC_SANITY_DATASET ||
-  (() => {
-    throw new Error("Missing: NEXT_PUBLIC_SANITY_DATASET");
-  })();
-
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-10-01";
-
-export const useCdn =
-  process.env.NEXT_PUBLIC_SANITY_USE_CDN === "true";
+export const projectId = "tzp4zvm3"; // replace with actual projectId
+export const dataset = "production";
+export const apiVersion = "2023-10-01";
+export const useCdn = true;
